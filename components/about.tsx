493a46b7bb0ee2899e4080205860dc3aaa3f24c4
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-06-30 01:23:22 -0600

    Initial commit

---
+++ components/about.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        {/* After graduating with a degree in{" "}
+        <span className="font-medium">Accounting</span>, I decided to pursue my
+        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB
+        </span>
+        . I am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer. */}
+        I'm currently studying Computer Science at the <span className="font-bold">
+          University of Calgary
+        </span>, where I've had the opportunity to develop a strong foundation in various aspects of technology. My academic journey has been driven by a keen interest in <span className="font-bold">
+        data analytics and visualization
+        </span>, which is where I believe the future of technology lies. I've become proficient in SQL for data handling and Power BI for creating insightful visualizations, skills that I've honed through both academic projects and personal initiatives. In addition to my academic pursuits, I've gained practical experience working with MERN ( <span className="font-bold">
+          MongoDB, Express.js, Node.js, React
+        </span> ) Stack. Whether it's through data analytics, providing meaningful insights for business decisions, or developing applications that can make people's lives easier, I am passionate about using my skills to make a tangible impact.
+      </p>
+      
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games and piano, watching movies, and animes. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">machine learning algorithms to implement in my MERN Stack projects.</span>
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,121 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import Summarify from "@/public/Summarify.png";
+import Tokyo from "@/public/Tokyo Olympic 2021 Analysis.png";
+import Atliq from "@/public/Atliq Sales Analysis.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Education",
+    hash: "#education",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const educationData = [
+  {
+    title: "University of Calgary",
+    location: "Calgary, AB",
+    description:
+      "I'm currently pursuing a Bachelor of Science in Computer Science at the University of Calgary. I'm expected to graduate in 2025",
+    icon: React.createElement(LuGraduationCap),
+    date: "2020 - Present",
+  },
+  {
+    title: "Notre Dame College",
+    location: "Dhaka, Bangladesh",
+    description:
+      "I completed my High School from Notre Dame College, one of the most prestigious schools in Bangladesh. I studied Mathematics, Physics, Biology, and Chemistry.",
+      icon: React.createElement(LuGraduationCap),
+      date: "2017 - 2019",
+  },
+  // {
+  //   title: "Full-Stack Developer",
+  //   location: "Houston, TX",
+  //   description:
+  //     "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+  //   icon: React.createElement(FaReact),
+  //   date: "2021 - present",
+  // },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Summarify",
+    description:
+      "Revolutionizing study methods to boost academic achievements and reduce study time.",
+    tags: ["HTML", "CSS", "Javascript", "Express.js","React.js", "Typescript", "MongoDB", "Material UI",],
+    imageUrl: Summarify,
+  },
+  {
+    title: "Tokyo 2021 Olympics Data Analysis",
+    description:
+      "The goal of this project was to leverage advanced data analytics to uncover valuable insights from the Tokyo 2021 Olympics data.",
+    tags: ["Synapse Analytics", "Azure Databricks", "MySQL", "Apache Spark", "Power BI"],
+    imageUrl: Tokyo,
+  },
+  {
+    title: "Atliq Company Sales Analysis",
+    description:
+      "The objective of this project was to analyze Atliq Company's sales data from 2017 to 2020 to identify trends and key performance indicators. ",
+    tags: ["MySQL", "Power BI"],
+    imageUrl: Atliq,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Express.js",
+  "Git",
+  "Material UI",
+  "Bootstrap5",
+  "MongoDB",
+  "Docker",
+  "Power BI",
+  "Tableau",
+  "MySQL",
+  "Microsoft Azure",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Microsoft Excel",
+  "Figma",
+  "Microsoft Powerpoint",
+  "Microsoft Word",
+  "Microsoft Outlook",
+  "Microsoft Teams",
+  "Active Directory",
+  "Windows Server",
+  "Linux",
+  "VPN",
+  "Network Security",
+  
+
+] as const;
