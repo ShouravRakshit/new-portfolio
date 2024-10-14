32ab50c74546f9068bfc5bb06727c602e5e16637
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-10-13 21:42:31 -0600

    fixed the about section.

--- components/about.tsx
+++ components/about.tsx
@@ -19,21 +19,6 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        {/* After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer. */}
         I'm currently studying Computer Science at the <span className="font-bold">
           University of Calgary
         </span>, where I've had the opportunity to develop a strong foundation in various aspects of technology. My academic journey has been driven by a keen interest in <span className="font-bold">
